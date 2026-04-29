ca486e3396ad023da35009d61596de107eed7db7
karthikprasanna07 <pkarthikprasanna7@gmail.com>
2026-04-29 16:38:46 +0530

    Added a subarray prblm

---
+++ src/HashMap/SubArraySum.java
@@ -0,0 +1,13 @@
+package HashMap;
+
+public class SubArraySum {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3};
+        for (int i = 0 ; i < arr.length ; i++) { // start
+            for (int j = i; j < arr.length ; j++) { // end
+
+            }
+
+        }
+    }
+}
